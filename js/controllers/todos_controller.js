51b647ee7d3ce5b9104eb77b8202e97fc36675d7
patricksimpson <izerop@gmail.com>
2014-02-16 22:06:50 -0500

    check all

--- js/controllers/todos_controller.js
+++ js/controllers/todos_controller.js
@@ -33,6 +33,12 @@ Todos.TodosController = Ember.ArrayController.extend({
     return remaining === 1 ? 'item' : 'items';
   }.property('remaining'),
   allAreDone: function(key, value) {
-    return !!this.get('length') && this.everyProperty('isCompleted', true);
+    if (value === undefined) {
+      return !!this.get('length') && this.everyProperty('isCompleted', true);
+    } else { 
+      this.setEach('isCompleted', value);
+      this.invoke('save');
+      return value;
+    }
   }.property('@each.isCompleted')
 });
